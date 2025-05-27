df31aa5fe3841f1dc369cfc868ed083164c08fc8
lev <lev@kermy.org>
2025-05-27 15:35:15 -0300

    v1.16.1

--- pkg/common/config.go
+++ pkg/common/config.go
@@ -31,6 +31,7 @@ type GPTConfig struct {
 
 type WebConfig struct {
 	Land   string `mapstructure:"land"`
+	Widget string `mapstructure:"widget"`
 	TgBot  string `mapstructure:"tgbot"`
 	TgUser string `mapstructure:"tguser"`
 	Whats  string `mapstructure:"whats"`
