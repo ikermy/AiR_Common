a037a7c100f7e2b5ba908d4a95e0ffc817ea2a96
lev <lev@kermy.org>
2025-07-02 13:32:05 -0300

    v1.20.5

--- pkg/comdb/db.go
+++ pkg/comdb/db.go
@@ -17,6 +17,11 @@ type Message struct {
 	Message   string      `json:"message"`
 	Timestamp time.Time   `json:"timestamp"`
 }
+type Espero struct {
+	Limit  uint16 `json:"limit"`
+	Wait   uint8  `json:"wait"`
+	Ignore bool   `json:"ignore"`
+}
 
 const (
 	AI        CreatorType = 1
@@ -25,11 +30,11 @@ const (
 )
 
 // DecompressAndExtractMetadata Функция для распаковки сжатых данных и извлечения полей Meta и MetaAction
-func DecompressAndExtractMetadata(compressedData []byte) (string, []string, error) {
+func DecompressAndExtractMetadata(compressedData []byte) (string, []string, *Espero, error) {
 	// Создаем reader для распаковки данных
 	gzipReader, err := gzip.NewReader(bytes.NewReader(compressedData))
 	if err != nil {
-		return "", nil, fmt.Errorf("ошибка при создании gzip reader: %w", err)
+		return "", nil, nil, fmt.Errorf("ошибка при создании gzip reader: %w", err)
 	}
 	defer func(gzipReader *gzip.Reader) {
 		err := gzipReader.Close()
@@ -41,18 +46,19 @@ func DecompressAndExtractMetadata(compressedData []byte) (string, []string, erro
 	// Читаем распакованные данные
 	decompressedData, err := io.ReadAll(gzipReader)
 	if err != nil {
-		return "", nil, fmt.Errorf("ошибка при распаковке данных: %w", err)
+		return "", nil, nil, fmt.Errorf("ошибка при распаковке данных: %w", err)
 	}
 
 	// Разбираем JSON
 	var modelData map[string]interface{}
 	if err := json.Unmarshal(decompressedData, &modelData); err != nil {
-		return "", nil, fmt.Errorf("ошибка при разборе JSON модели: %w", err)
+		return "", nil, nil, fmt.Errorf("ошибка при разборе JSON модели: %w", err)
 	}
 
 	// Извлекаем поля MetaAction
 	var metaAction string
 	var triggers []string
+	var espero *Espero = &Espero{}
 
 	if ma, ok := modelData["mact"].(string); ok {
 		metaAction = ma
@@ -69,5 +75,18 @@ func DecompressAndExtractMetadata(compressedData []byte) (string, []string, erro
 		}
 	}
 
-	return metaAction, triggers, nil
+	// Извлекаем поля espero
+	if esp, ok := modelData["espero"].(map[string]interface{}); ok {
+		if limit, ok := esp["limit"].(float64); ok {
+			espero.Limit = uint16(limit)
+		}
+		if wait, ok := esp["wait"].(float64); ok {
+			espero.Wait = uint8(wait)
+		}
+		if ignore, ok := esp["ignore"].(bool); ok {
+			espero.Ignore = ignore
+		}
+	}
+
+	return metaAction, triggers, espero, nil
 }
